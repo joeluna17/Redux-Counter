a640368047d177a492238c0ee7a1c0ffcb80360c
Joseph Luna <josephluna@Josephs-MBP.local>
2019-06-19 19:21:13 -0500

    intial commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -39,6 +39,7 @@ class Counter extends Component {
     }
 }
 
+
 // The mapStateToProps function specifies which portion of the
 // state tree this component needs to receive. In this case,
 // since our redux store is only storing the value of the count,
