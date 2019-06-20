39bf403a2e4d314c01d9d370285e2ce2113790ce
Joseph Luna <josephluna@Josephs-MBP.local>
2019-06-19 20:26:48 -0500

    committing MVP functionality plus incrementIfOdd method

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
-export const INCREMENT = 'INCREMENT';
+export const INCREMENT = 'INCREMENT'; // action type
 export const DECREMENT = 'DECREMENT';
+export const INCREMENT_IF_ODD = 'INCREMENT_IF_ODD';
 
 // Our action creators will return
 // an action packet that our reducer will
@@ -9,10 +10,28 @@ export const DECREMENT = 'DECREMENT';
 // logic of updating the central Redux store. That
 // is left to the reducer(s).
 
-export const increment = () => {
+export const increment = (/*somePayload*/) => { // action creator
   // Fill in this function
+  console.log("Hello from inside increment method in the actions/index.js file action creator.")
+      return( // action
+          {
+            type: INCREMENT,
+         // payload: somePayload
+          }
+      )
 };
 
 export const decrement = () => {
   // Fill in this function
+  console.log("Hello from inside decrement method in the actions/index.js file action creator.")
+    return{
+        type: DECREMENT,
+    }
 };
+
+
+export const incrementIfOdd = () => {
+    return {
+      type: INCREMENT_IF_ODD,
+    }
+}
